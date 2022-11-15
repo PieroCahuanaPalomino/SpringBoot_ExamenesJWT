b15bd671a571a988ff365408f3c5d0eaa81383b9
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-11-15 13:27:00 -0500

    UserDetailServiceImpl implementa UserDetailsService

---
+++ src/main/java/sistema/examenes/spring/configuraciones/JwtAuthenticationFilter.java
@@ -0,0 +1,30 @@
+package sistema.examenes.spring.configuraciones;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import sistema.examenes.spring.servicios.impl.UserDetailsServiceImpl;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/sistema/examenes/spring/servicios/impl/UserDetailsServiceImpl.java
+++ src/main/java/sistema/examenes/spring/servicios/impl/UserDetailsServiceImpl.java
@@ -1,7 +1,29 @@
 package sistema.examenes.spring.servicios.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import sistema.examenes.spring.entidades.Usuario;
+import sistema.examenes.spring.repositorios.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	
+	//impl
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario=this.usuarioRepository.findByUsername(username);
+		if(usuario==null) {
+			throw new UsernameNotFoundException("Usuario no encontrado");
+		}
+		return usuario;
+	}
+
+}
